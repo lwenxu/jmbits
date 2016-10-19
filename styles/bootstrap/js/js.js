7a0ba184d2c98f2eca10747720abd5e4ee2c523f
lwen <1405766743@qq.com>
2016-10-19 22:59:44 +0800

    first commit nwupt to gitlab

---
+++ styles/bootstrap/js/js.js
@@ -0,0 +1,5 @@
+/**
+ * Created by lwen on 2016/10/16.
+ */
+var table=document.getElementsByTagName('table');
+table.classList.add('table');
---
+++ styles/js/js.js
@@ -0,0 +1,5 @@
+/**
+ * Created by lwen on 2016/10/16.
+ */
+
+document.getElementsByName('table').classList.add('table');
